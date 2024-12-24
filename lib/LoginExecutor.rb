39a68d7047be0c9c36f3be3b3d26776926cc7d25
Dominik Poncza <dominik.poncza@juicymo.cz>
2024-12-25 00:01:03 +0100

    Edit style

--- lib/LoginExecutor.rb
+++ lib/LoginExecutor.rb
@@ -1,32 +1,30 @@
 module LoginCommands
 
-########################################################################################
-     private
+      private
+
       def login()
-            response = @session.post('https://steamcommunity.com/login/getrsakey/', {'username' => @username}).content
-            data = pass_stamp(response,@password)
+            response = @session.post('https://steamcommunity.com/login/getrsakey/', { 'username' => @username }).content
+            data = pass_stamp(response, @password)
             encrypted_password = data["password"]
             timestamp = data["timestamp"]
-            repeater = 0
-
 
             send = {
-                  'password' => encrypted_password,
-                  'username' => @username,
-                  'twofactorcode' =>'', #update
-                  'emailauth' => '',
-                  'loginfriendlyname' => '',
-                  'captchagid' => '-1',
-                  'captcha_text' => '',
-                  'emailsteamid' => '',
-                  'rsatimestamp' => timestamp,
-                  'remember_login' => @remember
+              'password' => encrypted_password,
+              'username' => @username,
+              'twofactorcode' => '',
+              'emailauth' => '',
+              'loginfriendlyname' => '',
+              'captchagid' => '-1',
+              'captcha_text' => '',
+              'emailsteamid' => '',
+              'rsatimestamp' => timestamp,
+              'remember_login' => @remember
             }
+
             login = @session.post('https://steamcommunity.com/login/dologin', send).content
             firstreq = JSON.parse(login)
 
             raise "Incorrect username or password" if firstreq["message"] == "The account name or password that you have entered is incorrect."
-
 
             until firstreq["success"] == true
                   sleep(0.3)
@@ -34,13 +32,14 @@ module LoginCommands
                   cap = ''
                   if firstreq['captcha_needed'] == true
                         gid = firstreq['captcha_needed']
-                        File.delete("./#{username}_captcha.png") if File.exist?("./#{username}_captcha.png")
+                        File.delete("./#{@username}_captcha.png") if File.exist?("./#{@username}_captcha.png")
                         @session.get("https://steamcommunity.com/login/rendercaptcha?gid=#{gid}").save "./#{@username}_captcha.png"
                         puts "you need to write a captcha to continue"
                         puts "there is an image named #{@username}_captcha in the script directory"
-                        puts "open it and write the captha here"
+                        puts "open it and write the captcha here"
                         cap = gets.chomp
                   end
+
                   emailauth = ''
                   facode = ''
                   emailsteamid = ''
@@ -49,7 +48,7 @@ module LoginCommands
                               puts "write 2FA code"
                               facode = gets.chomp
                         else
-                              facode = fa(@secret,@time_difference)
+                              facode = fa(@secret, @time_difference)
                         end
                   elsif firstreq['emailauth_needed'] == true
                         emailsteamid = firstreq['emailsteamid']
@@ -59,98 +58,79 @@ module LoginCommands
                   end
 
                   send = {
-                        'password' => encrypted_password,
-                        'username' => @username,
-                        'twofactorcode' => facode, #update
-                        'emailauth' => emailauth,
-                        'loginfriendlyname' => '',
-                        'captchagid' => gid,
-                        'captcha_text' => cap,
-                        'emailsteamid' => emailsteamid,
-                        'rsatimestamp' => timestamp,
-                        'remember_login' => @remember
+                    'password' => encrypted_password,
+                    'username' => @username,
+                    'twofactorcode' => facode,
+                    'emailauth' => emailauth,
+                    'loginfriendlyname' => '',
+                    'captchagid' => gid,
+                    'captcha_text' => cap,
+                    'emailsteamid' => emailsteamid,
+                    'rsatimestamp' => timestamp,
+                    'remember_login' => @remember
                   }
-                  output "attempting to login"
-                  login = @session.post('https://steamcommunity.com/login/dologin', send ).content
-                  firstreq = JSON.parse(login)
 
+                  output "attempting to login"
+                  login = @session.post('https://steamcommunity.com/login/dologin', send).content
+                  firstreq = JSON.parse(login)
             end
+
             response = firstreq
 
-
-
-
-
-            if @steamid != nil && @steamid != response["transfer_parameters"]["steamid"]
+            if @steamid && @steamid != response["transfer_parameters"]["steamid"]
                   puts "the steamid you provided does not belong to the account you entered"
                   puts "steamid will be overwritten"
                   @steamid = response["transfer_parameters"]["steamid"]
-
             else
                   @steamid = response["transfer_parameters"]["steamid"]
             end
 
-
-            response["transfer_urls"].each { |url|
+            response["transfer_urls"].each do |url|
                   @session.post(url, response["transfer_parameters"])
-            }
-
-
+            end
 
             steampowered_sessionid = ''
-            @session.cookies.each { |c|
-                  if c.name == "sessionid"
-                         steampowered_sessionid = c.value
-                   end
-            }
+            @session.cookies.each do |c|
+                  steampowered_sessionid = c.value if c.name == "sessionid"
+            end
 
-            cookie = Mechanize::Cookie.new :domain => 'steamcommunity.com', :name =>'sessionid', :value =>steampowered_sessionid, :path => '/'
+            cookie = Mechanize::Cookie.new domain: 'steamcommunity.com', name: 'sessionid', value: steampowered_sessionid, path: '/'
             @session.cookie_jar << cookie
             @loggedin = true
+
             begin
                   text = Nokogiri::HTML(@session.get("https://steamcommunity.com/dev/apikey").content).css('#bodyContents_ex').css('p').first.text.split(' ')
-                  if text.include?('Registering for a Steam Web API Key will enable you to access many Steam features from your own website') == false
-                        @api_key = text[1]
-                  end
+                  @api_key = text[1] unless text.include?('Registering for a Steam Web API Key will enable you to access many Steam features from your own website')
             rescue
                   output "Could not retrieve api_key"
             end
 
-            if !@api_key.nil?
-                  data = get_player_summaries(@steamid) if !@api_key.nil?
-                  data.each { |element|
-                        if element["steamid"].to_s == @steamid.to_s
-                              @persona = element["personaname"]
-                        end
-                  }
+            if @api_key
+                  data = get_player_summaries(@steamid)
+                  data.each do |element|
+                        @persona = element["personaname"] if element["steamid"].to_s == @steamid.to_s
+                  end
             end
+
             output "logged in as #{@persona}"
             output "your steamid is #{@steamid}"
-            output "loaded API_KEY : #{@api_key}" if !@api_key.nil?
+            output "loaded API_KEY: #{@api_key}" if @api_key
       end
-########################################################################################
 
-
-########################################################################################
-      def pass_stamp(give,password)
-
-            data = JSON::parse(give)
+      def pass_stamp(give, password)
+            data = JSON.parse(give)
             mod = data["publickey_mod"].hex
             exp = data["publickey_exp"].hex
             timestamp = data["timestamp"]
 
-            key   = OpenSSL::PKey::RSA.new
-            if RUBY_VERSION.to_f <= 2.3
-                  key.e = OpenSSL::BN.new(exp)
-                  key.n = OpenSSL::BN.new(mod)
-            elsif RUBY_VERSION.to_f >= 2.4
-                  #key.set_key(n, e, d)
-                  key.set_key(OpenSSL::BN.new(mod), OpenSSL::BN.new(exp),nil)
-            end
-            ep = Base64.encode64(key.public_encrypt(password.force_encoding("utf-8"))).gsub("\n", '')
-            return {'password' => ep, 'timestamp' => timestamp }
-      end
-########################################################################################
+            asn1 = OpenSSL::ASN1::Sequence([
+                                             OpenSSL::ASN1::Integer(mod),
+                                             OpenSSL::ASN1::Integer(exp)
+                                           ])
+            rsa_key = OpenSSL::PKey::RSA.new(asn1.to_der)
 
+            ep = Base64.encode64(rsa_key.public_encrypt(password.encode("utf-8"))).gsub("\n", '')
+            { 'password' => ep, 'timestamp' => timestamp }
+      end
 
 end
